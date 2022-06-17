dafc3699df2af8bbeb4894b27d8400f2a4fd5849
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-17 13:14:18 +0300

    commit tree 2c759e38caf35adcac71c28dd714bbad61a17b7d

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+const form = document.querySelector("form")
+const userName = document.getElementById('userName')
+const email = document.getElementById('email')
+
+const names=document.getElementById("name")
+const mail=document.getElementById("mail")
+const id=document.getElementById("id")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    submitData(userName.value,email.value)
+})
+
+function submitData(userName, email) {
+    const jsonObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({ name: userName, email: email })
+    }
+
+    return fetch("http://localhost:3000/users", jsonObj)
+        .then(response => response.json())
+        .then(jsonData=>{
+            names.textContent=`Name: ${jsonData.name}`
+            mail.textContent=`Email: ${jsonData.email}`
+            id.textContent=`Id: ${jsonData.id}`
+        })
+        .catch(error=> {
+        alert("Bad things have happened Mr. Poodle!")
+        console.log(error.message)
+    })
+}
+
+
+
+
+
